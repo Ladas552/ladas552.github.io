dd92bf93a4747d1d975bea7dc86e5794b76ad417
Ladas552 <l.tokshalov@gmail.com>
2025-03-04 13:06:59 +0500

    Flake that adds norgolith to shell

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "Shell with norgolith";
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
@@ -13,10 +13,18 @@
       nixpkgs,
       norgolith,
       ...
-    }@inputs:
+    }:
     {
 
       packages.x86_64-linux.default = norgolith.packages.x86_64-linux.default;
-
+      devShells.x86_64-linux =
+        let
+          pkgs = nixpkgs.legacyPackages.x86_64-linux;
+        in
+        {
+          default = pkgs.mkShell {
+            packages = [ self.packages.x86_64-linux.default ];
+          };
+        };
     };
 }
