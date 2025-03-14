bf9e78db12d179a0efa9d5b7b5e0c14c20c3e08a
Ladas552 <l.tokshalov@gmail.com>
2025-03-14 16:21:32 +0500

    more scripts and aliases the work one is in progress, needs niri ipc

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Shell with norgolith";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
 
     norgolith.url = "github:NTBBloodbath/norgolith";
   };
@@ -23,7 +23,24 @@
         in
         {
           default = pkgs.mkShell {
-            packages = [ self.packages.x86_64-linux.default ];
+            packages = [
+              self.packages.x86_64-linux.default
+              (pkgs.writeShellScriptBin "ser" ''lith serve --drafts'')
+              (pkgs.writeShellScriptBin "serd" ''lith serve --no-drafts'')
+              (pkgs.writeShellScriptBin "build" ''lith build -m'')
+              (pkgs.writeShellScriptBin "new" ''lith new -k norg posts/$1'')
+              (pkgs.writeShellScriptBin "update" ''nix flake update'')
+              (pkgs.writeShellScriptBin "work" # bash
+                ''
+                  ghostty -e "hx ./templates/" &
+                  ghostty -e "nvim ./content/" &
+                  chromium "http://localhost:3030/" "https://ladas552.github.io/" &
+                  ghostty -e "${pkgs.links2}/bin/links "http://localhost:3030/"" &
+                  ghostty -e "lith serve -d ~/Projects/my_repos/ladas552.github.io/Rattman"
+                  niri msg action close-window
+                ''
+              )
+            ];
           };
         };
     };
