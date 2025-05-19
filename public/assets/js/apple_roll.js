79a7a81ae05cb868a3e9e434c6db08716f762cc9
Ladas552 <l.tokshalov@gmail.com>
2025-05-19 12:19:59 +0500

    making it work

---
+++ public/assets/js/apple_roll.js
@@ -0,0 +1,1 @@
+let isSafari=navigator.vendor&&navigator.vendor.indexOf(`Apple`)>-1&&navigator.userAgent&&navigator.userAgent.indexOf(`CriOS`)==-1&&navigator.userAgent.indexOf(`FxiOS`)==-1;isSafari&&window.location.replace(`https://www.youtube.com/watch?v=dQw4w9WgXcQ`)
